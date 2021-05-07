5accef5ebb975b9b15718abdab5705ad8ced119b
Leonardo Lourenco <lnddru@gmail.com>
2021-05-07 11:32:01 -0500

    Add files via upload

---
+++ CursoJava.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package basico;
+
+/**
+ * @author leo
+ *
+ */
+public class CursoJava {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) { // programa sempre começa com o main
+		// TODO Auto-generated method stub
+		System.out.println("Hello Word");
+		
+		// Pulando uma linha  OBS; ln para pular linha // fazer sem a primeira vez
+		System.out.print("Curso Java Canal Coffee Tag");
+		
+
+	}
+
+}
