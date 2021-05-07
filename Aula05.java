c20a220a118dafa6a547cebb92738a631f8bb48f
Leonardo Lourenco <lnddru@gmail.com>
2021-05-07 11:32:13 -0500

    Add files via upload

---
+++ Aula05.java
@@ -0,0 +1,72 @@
+/**
+ * 
+ */
+package basico;
+
+/**
+ * @author leo
+ *
+ */
+public class Aula05 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// Vamos ver as mais usadadas:
+		// São 4 formas de definir variaveis do tipo INT:
+		// Única coisa que muda de uma para outra é seu tamanho e espaço que ocupa na memória 
+		// do computador
+		
+		byte ValorTipoByte = 124;
+		
+		short ValorTipoShort = 29895;
+		
+		int ValorTipoInt = 22454110;
+		
+		long ValorTipoLong = 224541104;
+		
+		System.out.println("Valor informado: " + ValorTipoByte);
+		System.out.println("Valor informado: " + ValorTipoShort);
+		System.out.println("Valor informado: " + ValorTipoInt);
+		System.out.println("Valor informado: " + ValorTipoLong);
+		
+		
+		System.out.println("");
+		
+		// Varaveis com casas decimais:
+		
+		float ValorTipoFloat = 15.5f;  // sem o f da erro, o f é para dizer para o computador 
+		// que está variável é do tipo float
+		
+		double ValorTipoDouble = 20.5;
+		
+		System.out.println("Valor informado: " + ValorTipoFloat);
+		System.out.println("Valor informado: " + ValorTipoDouble);
+		
+		
+		System.out.println("");
+		
+		// Dados do Tipo Caracter:
+		
+		char texto = 'l'; // coloque um numero tambem 6
+		System.out.println("Letra informado: " + texto);
+		
+		
+		System.out.println("");
+		
+		// Variáveis do tipo Boolean:
+		
+		boolean decisao = true;  // colocar false 
+		System.out.println("Decisão e:" + decisao);
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
